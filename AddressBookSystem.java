768b242753b1087bba9f13202aeb34c83366f439
Pratik Lahamge <89896925+Pratiklahamge@users.noreply.github.com>
2021-10-27 17:37:33 +0530

    Ability to delete a person using person's name

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,121 +1,139 @@
 package com.briglabz;
+import java.util.ArrayList;
+import java.util.Scanner;
 /*
  * Purpose - To create an Address Book System
  * name _ pratik lahamge
  */
 
-class Contact{ // instances class 
-	private String firstName, lastName, address, city, state, email;
-	private int zip;
-	private long phoneNumber;
-
-	/*
-    Used Constructor to store the Parameter
-    Also used this keyword to make difference between class name variable and constructure name variable
-    because both the variables are same.
-	 */
-	public Contact(String firstName, String lastName, String address, String city, String state, String email, int zip, long phoneNumber) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.email = email;
-		this.zip = zip;
-		this.phoneNumber = phoneNumber;
-	}
-
-	/*
-    Used getter and setter to set and get the value.
-    Setter is used to set the value
-    Getter is used to get the value
-	 */
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public int getZip() {
-		return zip;
-	}
-
-	public void setZip(int zip) {
-		this.zip = zip;
-	}
-
-	public long getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(long phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-}
-
 public class AddressBookSystem {
-	static void welcome(){
-		System.out.println("Welcome to Address Book System Program\n");
+
+	ArrayList<Contacts> arrayDetails = new ArrayList<Contacts>();
+	static Scanner sc = new Scanner(System.in);
+
+	/**
+	 * This method is used to add details to address book
+	 */
+	public void addDetails() {
+		Contacts info = new Contacts();
+		System.out.println("Enter the first name");
+		info.setFirstName(sc.nextLine());
+		System.out.println("Enter the last name");
+		info.setLastName(sc.nextLine());
+		System.out.println("Enter the address");
+		info.setAddress(sc.nextLine());
+		System.out.println("Enter the city");
+		info.setCity(sc.nextLine());
+		System.out.println("Enter the state");
+		info.setState(sc.nextLine());
+		System.out.println("Enter the email");
+		info.setEmail(sc.nextLine());
+		System.out.println("Enter the zip code");
+		info.setZip(sc.nextInt());
+		System.out.println("Enter the phone number");
+		info.setPhoneNumber(sc.nextLong());
+		arrayDetails.add(info);
+		System.out.println(arrayDetails);
 	}
+
+	/**
+	 *This method is used to edit the details in address book
+	 */
+
+	public void editDetails() {
+		System.out.println("Confirm your first name to edit details: ");
+		String confirmName = sc.next();
+
+		for (int i = 0; i < arrayDetails.size(); i++) {
+			if (arrayDetails.get(i).getFirstName().equals(confirmName)) {
+				System.out.println("Select form below to change: ");
+				System.out.println("\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Zip\n7.Mobile number\n8.Email");
+				int edit = sc.nextInt();
+
+				switch (edit) {
+				case 1:
+					System.out.println("Enter first name");
+					arrayDetails.get(i).setFirstName(sc.next());
+					break;
+				case 2:
+					System.out.println("Enter Last name");
+					arrayDetails.get(i).setLastName(sc.next());
+					break;
+				case 3:
+					System.out.println("Enter Address");
+					arrayDetails.get(i).setAddress(sc.next());
+					break;
+				case 4:
+					System.out.println("Enter City");
+					arrayDetails.get(i).setCity(sc.next());
+					break;
+				case 5:
+					System.out.println("Enter State");
+					arrayDetails.get(i).setState(sc.next());
+					break;
+				case 6:
+					System.out.println("Enter Zip");
+					arrayDetails.get(i).setZip(sc.nextInt());
+					break;
+				case 7:
+					System.out.println("Enter Mobile number");
+					arrayDetails.get(i).setPhoneNumber(sc.nextLong());
+					break;
+				case 8:
+					System.out.println("Enter new E-mail");
+					arrayDetails.get(i).setEmail(sc.next());
+					break;
+				}
+				System.out.println("Edited list is: ");
+				System.out.println(arrayDetails);
+			} else
+				System.out.println("Enter a valid First name");
+		}
+	}
+
+	/**
+	 * This method is used to delete the contact details
+	 */
+	public void deleteDetails() {
+		System.out.println("Confirm the first name of the person to delete contact");
+		String confirmName = sc.next();
+		for (int i = 0; i < arrayDetails.size(); i++) {
+
+			if (arrayDetails.get(i).getFirstName().equals(confirmName)) {
+				arrayDetails.remove(i);
+				System.out.println("List After removing"+arrayDetails);
+
+			} else {
+				System.out.println("Enter valid first name");
+			}
+		}
+	}
+
 
 	public static void main(String[] args) {
-		welcome();
-		/*
-        We are just calling an object using class name.
-        Simply passing the argument value according to the parameter defined in the constructor.
-		 */
-		Contact details = new Contact("Pratik", "Lahamge", "karanja, washim", "maharastra",
-				"maharastra", "plahamge340@gmail.com", 285602, 7745852151L);
-		System.out.println("The following contact details is mentioned below : \n");
-		System.out.println("First Name : " + details.getFirstName());
-		System.out.println("Last Name  : " + details.getLastName());
-		System.out.println("Address    : " + details.getAddress());
-		System.out.println("City       : " + details.getCity());
-		System.out.println("State      : " + details.getState());
-		System.out.println("E-mail     : " + details.getEmail());
-		System.out.println("Zip Code   : " + details.getZip());
-		System.out.println("Phone No   : " + details.getPhoneNumber());
+		AddressBookSystem details = new AddressBookSystem();
+		details.addDetails();
+		int i = 0;
+		while (i == 0) {
+			System.out.println("Welcome to Address Book Program");
+			System.out.println("What do you want to do: ");
+			System.out.println("1.Add details.\n2.Edit details.\n3.Delete Details.");
+			int choose = sc.nextInt();
+			switch (choose) {
+			case 1:
+				details.addDetails();
+				break;
+			case 2:
+				details.editDetails();
+				break;
+			case 3:
+				details.deleteDetails();
+				break;
+			default:
+				i = 1;
+				System.out.println("Wrong option");
+				break;
+			}
+		}
 	}
 }
