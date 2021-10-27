54d1948d7390f8aa3931258d97dcae019cd8def6
Pratik Lahamge <89896925+Pratiklahamge@users.noreply.github.com>
2021-10-27 16:29:47 +0530

    Ability to create a Contacts in Address
    Ability to create a Contacts in Address
    Book with first and last names, address,
    city, state, zip, phone number and
    email...

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,55 +1,121 @@
 package com.briglabz;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 /*
  * Purpose - To create an Address Book System
  * name _ pratik lahamge
  */
 
-public class AddressBookSystem {
+class Contact{ // instances class 
+	private String firstName, lastName, address, city, state, email;
+	private int zip;
+	private long phoneNumber;
 
-	ArrayList<Contacts> arrayDetails = new ArrayList<Contacts>();
-	Scanner sc = new Scanner(System.in);
-
-	/**
-	 * This method is used to add details to address book
+	/*
+    Used Constructor to store the Parameter
+    Also used this keyword to make difference between class name variable and constructure name variable
+    because both the variables are same.
 	 */
-	public void addDetails() {
-		Contacts info = new Contacts();
-		System.out.println("Enter the first name");
-		info.setFirstName(sc.nextLine());
-		System.out.println("Enter the last name");
-		info.setLastName(sc.nextLine());
-		System.out.println("Enter the address");
-		info.setAddress(sc.nextLine());
-		System.out.println("Enter the city");
-		info.setCity(sc.nextLine());
-		System.out.println("Enter the state");
-		info.setState(sc.nextLine());
-		System.out.println("Enter the email");
-		info.setEmail(sc.nextLine());
-		System.out.println("Enter the zip code");
-		info.setZip(sc.nextInt());
-		System.out.println("Enter the phone number");
-		info.setPhoneNumber(sc.nextLong());
-		arrayDetails.add(info);
-		sc.close();
+	public Contact(String firstName, String lastName, String address, String city, String state, String email, int zip, long phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.email = email;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
 	}
 
-	/**
-	 * This method is used to display the added information
+	/*
+    Used getter and setter to set and get the value.
+    Setter is used to set the value
+    Getter is used to get the value
 	 */
-	public void display() {
-		System.out.println(arrayDetails);
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program");
-		AddressBookSystem details = new AddressBookSystem();
-		details.addDetails();
-		details.display();
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
 }
 
+public class AddressBookSystem {
+	static void welcome(){
+		System.out.println("Welcome to Address Book System Program\n");
+	}
 
+	public static void main(String[] args) {
+		welcome();
+		/*
+        We are just calling an object using class name.
+        Simply passing the argument value according to the parameter defined in the constructor.
+		 */
+		Contact details = new Contact("Pratik", "Lahamge", "karanja, washim", "maharastra",
+				"maharastra", "plahamge340@gmail.com", 285602, 7745852151L);
+		System.out.println("The following contact details is mentioned below : \n");
+		System.out.println("First Name : " + details.getFirstName());
+		System.out.println("Last Name  : " + details.getLastName());
+		System.out.println("Address    : " + details.getAddress());
+		System.out.println("City       : " + details.getCity());
+		System.out.println("State      : " + details.getState());
+		System.out.println("E-mail     : " + details.getEmail());
+		System.out.println("Zip Code   : " + details.getZip());
+		System.out.println("Phone No   : " + details.getPhoneNumber());
+	}
+}
